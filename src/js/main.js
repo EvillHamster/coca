c1e030087b1bc4dbf692b4560c3de75001023886
Aidar <xamitgan@gmail.com>
2026-04-05 16:38:40 +0300

    Created pricing:hero section

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,10 @@ import '../assets/scss/main.scss';
 // ! components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import { UseInsightSlider, UseTestimonialsSlider } from './components/home/slider.js';
+import {
+  UseInsightSlider,
+  UseTestimonialsSlider,
+} from './components/home/slider.js';
 
 // ! activated functions
 useTheme();
