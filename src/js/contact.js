a14267a325c5f32a6129a5079cfe70318dbf934a
Aidar <xamitgan@gmail.com>
2026-04-10 15:22:38 +0300

    Created contact:getsarted section

--- src/js/contact.js
+++ src/js/contact.js
@@ -3,7 +3,9 @@ import '../assets/scss/contact.scss';
 // ! components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePhone } from './components/contact/phone.js'
 
 // ! activated functions
 useTheme();
 useBurger();
+usePhone();
