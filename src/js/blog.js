9aad8b261bb3e16f2d3df1bc8247c9886ee2866f
U-KALUGA\klg.hamitov <klg.hamitov@K4L3510W.kaluga.srsh.ru>
2026-04-20 13:05:23 +0300

    Created blog:articles.section + fix hero.section

--- src/js/blog.js
+++ src/js/blog.js
@@ -3,8 +3,10 @@ import '/scss/blog.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useBlogSliders } from './components/blog/heroSlider.js';
+import { useArticlesSlider } from './components/blog/articles-slider.js';
 
 // ! activated functions
 useTheme();
 useBurger();
 useBlogSliders();
+useArticlesSlider();
